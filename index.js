aca6eff79c33a90fde7e1f56eec9f20d1ac5f105
José Rodolpho <j.rodolpho29@hotmail.com>
2022-05-03 09:21:47 -0300

    Private messages filter created

--- index.js
+++ index.js
@@ -32,28 +32,36 @@ app.post("/participants", (req,res) =>{
     if(validation.error){
         res.status(422).send("Preencha corretamente!")
     }
-    else{
-        req.body.lastStatus = Date.now()
-        dataBase.collection("participants").insertOne(req.body)
-        const time = dayjs(req.body.lastStatus)
-        if(time.$m < 10){
-            time.$m = "0" + time.$m
+    dataBase.collection("participants").findOne({
+        name: req.body.name
+    }).then(user => {
+        if(user){
+            res.sendStatus(409)
         }
-        if(time.$s < 10){
-            time.$s = "0" + time.$s
+        else{
+            req.body.lastStatus = Date.now()
+            dataBase.collection("participants").insertOne(req.body)
+            const time = dayjs(req.body.lastStatus)
+            if(time.$m < 10){
+                time.$m = "0" + time.$m
+            }
+            if(time.$s < 10){
+                time.$s = "0" + time.$s
+            }
+            if(time.$H < 10){
+                time.$H = "0" + time.$H
+            }
+            dataBase.collection("messages").insertOne({
+                from: req.body.name,
+                to: "Todos",
+                text: "entra na sala...",
+                type: "status",
+                time: `${time.$H}:${time.$m}:${time.$s}`
+            })
+            
+            res.sendStatus(201)
         }
-        if(time.$H < 10){
-            time.$H = "0" + time.$H
-        }
-        dataBase.collection("messages").insertOne({
-            from: req.body.name,
-            to: "Todos",
-            text: "entra na sala...",
-            type: "status",
-            time: `${time.$H}:${time.$m}:${time.$s}`
-        })
-        res.sendStatus(201)
-    }
+    })
 })
 
 app.get("/participants", (req,res) => {
@@ -87,8 +95,14 @@ app.post("/messages", (req,res) => {
 
 app.get("/messages", (req,res) => {
     const limit = req.query.limit
+    const user = req.headers.user
     if(!limit){
         dataBase.collection("messages").find().toArray().then(message => {
+            message.forEach(filterMessage => {
+                if(filterMessage.to !== "Todos" && filterMessage.from !== user && filterMessage.to !== user){
+                    message.splice(0, message.indexOf(filterMessage))
+                }
+            })
             res.send(message)
         })
     }
@@ -96,6 +110,11 @@ app.get("/messages", (req,res) => {
         dataBase.collection("messages").find().toArray().then(message => {
             if(message.length > 50){
                 message.splice(0, (message.length - 50))
+                message.forEach(filterMessage => {
+                    if(filterMessage.to !== "Todos" && filterMessage.from !== user && filterMessage.to !== user){
+                        message.splice(message.indexOf(filterMessage), 1)
+                    }
+                })
                 res.send(message)
             }
         })
@@ -112,8 +131,9 @@ app.post("/status", (req,res) => {
                     _id: user._id},
                     {$set: {
                         lastStatus: Math.round(new Date().getTime())
-                    }})
-                res.sendStatus(200)
+                    }}).then(
+                        res.sendStatus(200)
+                    )
             }
         })
     })
@@ -123,7 +143,6 @@ function status(){
     dataBase.collection("participants").find().toArray().then(users =>{
         users.forEach(user =>{
             if(Math.round(user.lastStatus/1000 + 10) < Math.round(new Date().getTime()/1000.0) ){
-                console.log(Math.round(user.lastStatus/1000), Math.round(new Date().getTime()/1000.0))
                 dataBase.collection("participants").deleteOne(user)
                 const time = dayjs(user.lastStatus)
                 if(time.$m < 10){
